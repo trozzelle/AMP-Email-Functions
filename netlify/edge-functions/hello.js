b579e0254e927a448205b4af1ccc3b64b390decf
trozzelle <75337241+trozzelle@users.noreply.github.com>
2023-03-04 22:47:26 -0500

    -Init

---
+++ netlify/edge-functions/hello.js
@@ -0,0 +1,8 @@
+export default async (request) => {
+    return new Response("Hello, World!", {
+      headers: { "content-type": "text/html" },
+    });
+  };
+  
+
+export const config = { path: "/test" }
